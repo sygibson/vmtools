71397ba726a8c14b0b656b124d7b5b506ff88836
Shane Gibson <sygibson@gmail.com>
2019-04-04 02:19:37 +0000

    updated to drpjoin and docker run

--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ -z "$1" ]]
+then
+	[[ -z "$RS_ENDPOINT" ]] && read -p "Enter DRP Endpoint: " DRP || DRP=$RS_ENDPOINT
+else
+	DRP=$1
+	shift
+fi
 
-[[ -z "$RS_ENDPOINT" ]] && read -p "Enter DRP Endpoint: " RS
+# fixup if we get HTTPS API endpoint version - to use the files
+# on the HTTP
+DRP=$(echo $DRP | sed -e 's/https:/http:/g' -e 's/:8092/:8091/g')
+
 EXTRA_ARGS="$*"
 
 set -x
-docker run $EXTRA_ARGS -it --rm sygibson/vmtools /usr/bin/drpjoin $RS_ENDPOINT
+docker run $EXTRA_ARGS -it --rm sygibson/vmtools /usr/bin/drpjoin $DRP
 
